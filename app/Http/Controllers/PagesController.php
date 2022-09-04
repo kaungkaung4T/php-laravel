aed0852605a29efc6e59394113f5f2a8c0c1990c
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-09-04 18:01:36 +0630

    wfwefwefwef

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,6 +8,7 @@ class PagesController extends Controller
 {
 
     public function about() {
-        return view("template.work");
+        $name = "kaungkaungkaung";
+        return view("template.work", compact("name"));
     }
 }
--- resources/views/template/work.blade.php
+++ resources/views/template/work.blade.php
@@ -4,10 +4,13 @@
 
 
 @extends("layout.base")
-    
+
+
+
 @section("content")
     <h1>HELLO THRERE</h1>
 
+    <h2>{{$name}}</h2>
 
 
 @endsection
